6d1241cfaac4350e2f91e7481749097f3d10806c
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-05-26 12:30:07 -0300

    adicionando o envento de click.

--- app/src/main/java/com/gabrieldeespindula/conversordemoedas/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/conversordemoedas/MainActivity.java
@@ -2,11 +2,12 @@ package com.gabrieldeespindula.conversordemoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -19,7 +20,17 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate) {
+
+        }
+    }
+
     private static class ViewHolder {
         EditText editValue;
         TextView textDolar;
