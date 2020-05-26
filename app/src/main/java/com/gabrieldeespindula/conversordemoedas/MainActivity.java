dac30276a963043a168206b98ea727b3bbf56996
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-05-26 11:52:35 -0300

    Mapeamento dos elementos de interfece ultilizando o padrão ViewHolder.

--- app/src/main/java/com/gabrieldeespindula/conversordemoedas/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/conversordemoedas/MainActivity.java
@@ -2,16 +2,29 @@ package com.gabrieldeespindula.conversordemoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText valor = findViewById(R.id.editValor);
-        valor.getText();
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
+
     }
 }
